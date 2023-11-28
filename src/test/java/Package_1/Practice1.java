7575eb86c91c443740a069c5105ee9376197512b
rerishi <rishav3220@gmail.com>
2023-11-28 18:12:26 +0530

    First GitBash Commit

---
+++ src/test/java/Package_1/Practice1.java
@@ -0,0 +1,8 @@
+package Package_1;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello by Rishav");
+	}
+}
---
+++ src/test/java/Package_1/Practice2.java
@@ -0,0 +1,8 @@
+package Package_1;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello by Sarfraj");
+	}
+}
